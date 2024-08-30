397417fd26e1c82b910f55977df6b8e5c52c3af3
dejesusbg5 <dejesusbg5@gmail.com>
2024-08-29 23:52:55 -0500

    reupload file after accidental deletion

---
+++ src/lib/uniqueId.js
@@ -0,0 +1,7 @@
+function uniqueId() {
+  const time = Date.now().toString(36).slice(-4);
+  const rndm = Math.random().toString(36).substring(2, 4);
+  return time + rndm;
+}
+
+export default uniqueId;
