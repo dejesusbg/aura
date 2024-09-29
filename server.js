ca1950e1d91c1e18c81fb81a0394a1ef9ec52799
dejesusbg5 <dejesusbg5@gmail.com>
2024-09-29 12:11:33 -0500

    update npm start command

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
